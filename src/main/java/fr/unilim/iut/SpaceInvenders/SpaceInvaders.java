e0bbfd9ad2280960f803600bdcf81b988da2c2b8
KOULIBALY <bafode.koulibaly@etu.unilim.fr>
2021-05-02 16:25:52 +0200

    positionner un nouveau vaisseau

--- src/main/java/fr/unilim/iut/SpaceInvenders/SpaceInvaders.java
+++ src/main/java/fr/unilim/iut/SpaceInvenders/SpaceInvaders.java
@@ -1,8 +1,15 @@
 package fr.unilim.iut.SpaceInvenders;
 
+import fr.unilim.iut.SpaceInvaders.Vaisseau;
+import fr.unilim.iut.SpaceInvaders.utils.HorsEspaceJeuException;
+
 public class SpaceInvaders {
-    int longueur;
+    private static final String MARQUE_FIN_LIGNE = "\n";
+	private static final char MARQUE_VIDE = '.';
+	private static final char MARQUE_VAISSEAU = 'V';
+	int longueur;
     int hauteur;
+    Vaisseau vaisseau;
 
     public SpaceInvaders(int longueur, int hauteur) {
 	   this.longueur = longueur;
@@ -11,14 +18,46 @@ public class SpaceInvaders {
     
     @Override
     public String toString() {
+    	
 		StringBuilder espaceDeJeu = new StringBuilder();
-		for (int i = 0; i < hauteur; i++) {
-			for (int j = 0; j < longueur; j++) {
-				espaceDeJeu.append('.');
+		for (int y = 0; y < hauteur; y++) {
+			
+			for (int x = 0; x < longueur; x++) {
+				espaceDeJeu.append(recupererMarqueDeLaPosition(x, y));
 			}
-			espaceDeJeu.append('\n');
+			espaceDeJeu.append(MARQUE_FIN_LIGNE);
 		}
 		return espaceDeJeu.toString();
     }
+
+	private char recupererMarqueDeLaPosition(int x, int y) {
+		char marque;
+		if (this.aUnVaisseauQuiOccupeLaPosition(x, y))
+		      marque = MARQUE_VAISSEAU;
+		else
+		      marque = MARQUE_VIDE;
+		return marque;
+	}
+
+	private boolean aUnVaisseauQuiOccupeLaPosition(int x, int y) {
+		return this.aUnVaisseau() && vaisseau.occupeLaPosition(x, y);
+	}
+
+	private boolean aUnVaisseau() {
+		return vaisseau!=null;
+	}
+
+public void positionnerUnNouveauVaisseau(int x, int y) {
+		
+		if (  !estDansEspaceJeu(x, y) )
+			throw new HorsEspaceJeuException("La position du vaisseau est en dehors de l'espace jeu");
+	
+		vaisseau = new Vaisseau(x, y); 
+
+	}
+
+private boolean estDansEspaceJeu(int x, int y) {
+	return ((x >= 0) && (x < longueur)) && ((y >= 0) && (y < hauteur));
+}
 
 }
